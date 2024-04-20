45c52dcae482ef4bfcbf55364668727886f314c0
grilled-snakehead <snakehead@xmwyw.onmicrosoft.com>
2024-04-20 23:03:23 +0700

    add environment variables

--- config/database.js
+++ config/database.js
@@ -1,11 +1,18 @@
 /**
-* this configuration for connect our application with mongodb
-*/
-const mongoose = require('mongoose')
-let db = mongoose.connect('mongodb://localhost:27017/restaurant',{ useNewUrlParser: true } , (err)=> {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('connected...')
-    }
-})
+ * this configuration for connect our application with mongodb
+ */
+
+require("dotenv").config();
+
+const mongodb_url = process.env.MONGODB_URI;
+
+const mongoose = require("mongoose");
+let db = mongoose.connect(mongodb_url, { useNewUrlParser: true }, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("connected...");
+  }
+});
+
+module.exports = { db };
